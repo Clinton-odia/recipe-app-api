b6b174e458b3baff23c8f3dd609faba41ccf1bbc
Clinton <odiamehiclinton@gmail.com>
2025-07-03 15:22:50 +0100

    test and create the user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,21 @@
-from django.db import models  # noqa
+"""
+models data
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractBaseUser,
+    PermissionsMixin,
+)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """user model"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
